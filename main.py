ff78d267a2878f12da2ebda8b6c43a3b77aaa9d2
09b_Bácsi Barna <bacsi.barna@students.jedlik.eu>
2022-11-22 15:15:54 +0100

    4. menü

--- main.py
+++ main.py
@@ -13,6 +13,26 @@ while választás != '0':
     elif választás == '3':
         pass
     elif választás == '4':
-        pass
+        while len(lapjaid) != 0 and len(ellenfellapjai) != 0:
+            print("Előző lap: ",elozolap)
+            print("Lapjaid: "," ".join(lapjaid))
+            #print("Lapjai: "," ".join(ellenfellapjai))
+            if tudoklerakni():
+                lap = str(input("Adj meg egy lapot, amit le akarsz rakni: ").upper())
+                if lap in lapjaid:
+                    lapjaid.remove(lap)
+                    elozolap = lap
+                else:
+                    print("Nincs ilyen lapod, vagy nem tudsz ilyet lerakni!")
+                    while lap in lapjaid:
+                        lap = str(input("Adj meg egy lapot, amit le akarsz rakni: ").upper())
+                        if lap in lapjaid:
+                            lapjaid.remove(lap)
+                            elozolap = lap
+                        else:
+                            print("Nincs ilyen lapod, vagy nem tudsz ilyet lerakni!")
+            botlap = botlerakas()
+            if botlap != None:
+                elozolap = botlap
     elif választás == '5':
         pass
