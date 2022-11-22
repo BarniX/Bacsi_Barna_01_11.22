57416b35edd6a3396bce60bf2a4507a713cada07
Bácsi <bacsi.barna@students.jedlik.eu>
2022-11-22 10:11:01 +0100

    Alapok

--- functions.py
+++ functions.py
@@ -1,1 +1,11 @@
-alma
+from system import os
+
+def menu():
+    system('cls')
+    print('0 - Kilépés')
+    print('1 - Lapok kiírása')
+    print('2 - Útmutató')
+    print('3 - Játék elindítása (szabályos játékmenet')
+    print('4 - Játék elindítása (végtelen számú lapokkal')
+    print('5 - Játék elindítása (csak számos lapokkal')
+
