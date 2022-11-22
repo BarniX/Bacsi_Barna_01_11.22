450312eb112ec366e387789275cea6b7c142bbce
09b_Bácsi Barna <bacsi.barna@students.jedlik.eu>
2022-11-22 14:48:18 +0100

    Lapgenerálás

--- functions.py
+++ functions.py
@@ -1,4 +1,10 @@
 from os import system
+import random
+
+lapjaid = []
+ellenfellapjai = []
+elozolap = ""
+kezdolapok = 8
 
 def menu():
     system('cls')
@@ -9,3 +15,15 @@ def menu():
     print('4 - Játék elindítása (végtelen számú lapokkal')
     print('5 - Játék elindítása (csak számos lapokkal')
     return input('Válassz egy menüpontot: ')
+
+def lap_generalas():
+    szam = str(random.randint(0,9))s
+    szin = random.randint(1,4)
+    if szin == 1:
+        return "P" + szam
+    elif szin == 2:
+        return "Z" + szam
+    elif szin == 3:
+        return "S" + szam
+    elif szin == 4:
+        return "K" + szam
